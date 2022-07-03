be4ced9aa1e8fceabdd86725cf2526c107240a3c
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-03 15:41:29 +0530

    feat(project):create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const routes = [];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+});
+
+const app = Vue.createApp({});
+app.use(router);
+app.mount('#app');
